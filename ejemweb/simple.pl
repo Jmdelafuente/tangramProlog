dfa29859a7eb0f420115fc431278ce151b99e0e4
Juan M de la Fuente <Jmdelafuente22@users.noreply.github.com>
2017-10-18 19:28:37 -0300

    Ejemplos de Assert
    Ejemplos de la materia Lenguajes Declarativos sobre presistencia

---
+++ ejemweb/simple.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env /home/claudio/tmp/src/ciao-devel/build/bin/ciao-shell
+
+main(_) :-
+	write('Content-type: text/html'), nl,nl,
+	write('<HTML>'),
+	write('<b>Hello</b> world.'),
+	write('</HTML>').
+
